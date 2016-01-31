50a1ca62c9d82f30344edfa08b73a863d3c2eac0
Matt Dennis <msd7734@rit.edu>
2016-01-31 17:33:38 -0500

    Add _references.js for intellisense

---
+++ js/_references.js
@@ -0,0 +1,7 @@
+/// <reference path="plugins.js" />
+/// <reference path="rpg_core.js" />
+/// <reference path="rpg_managers.js" />
+/// <reference path="rpg_objects.js" />
+/// <reference path="rpg_scenes.js" />
+/// <reference path="rpg_sprites.js" />
+/// <reference path="rpg_windows.js" />
